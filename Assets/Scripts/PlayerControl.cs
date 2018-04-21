6bfe0066c8ab926ea23055790cecef144f33226d
armonistan <armonistan@gmail.com>
2018-04-21 16:28:07 -0500

    Added additional controls. To be refactored. Not where they need to be.
    Got collisions working again. (log statement atm)

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -14,10 +14,16 @@ public class PlayerControl : StatefulMonoBehavior<PlayerControl.States>
     }
 		
 	//directional controls
-	public KeyCode MoveLeft = KeyCode.A;
-	public KeyCode MoveRight = KeyCode.D;
-	public KeyCode MoveForward = KeyCode.W;
-	public KeyCode MoveBackward = KeyCode.S;
+	public KeyCode MoveLeft = KeyCode.LeftArrow;
+	public KeyCode MoveRight = KeyCode.RightArrow;
+	public KeyCode MoveForward = KeyCode.UpArrow;
+	public KeyCode MoveBackward = KeyCode.DownArrow;
+
+    //special moves
+    public KeyCode Spin = KeyCode.Q;
+    public KeyCode Sprint = KeyCode.W;
+    public KeyCode Juke = KeyCode.E;
+    public KeyCode Tackle = KeyCode.R;
 
 	//public States State = States.Running;
 
@@ -25,9 +31,11 @@ public class PlayerControl : StatefulMonoBehavior<PlayerControl.States>
     public float PlayerBaseSpeed = 4;
 	public float PlayerIdleSpeed = 0;
 
+    //private variables
 	private float _currentPlayerSpeed = 0;
 	private Vector2 _currentPlayerVector = new Vector2();
 	private float _radAngle = 0;
+    private bool _notSpinning = true;
 
 	//constants
 	private float _LEFT_X = -1;
@@ -35,10 +43,10 @@ public class PlayerControl : StatefulMonoBehavior<PlayerControl.States>
 	private float _FORWARD_Y = 1;
 	private float _BACKWARD_Y = -1;
 	private float _IDLE = 0;
+    private float _DEGREES_SPUN_PER_UPDATE = 90;
 
 
-
-	public Vector2 Velocity
+    public Vector2 Velocity
     {
         get
         {
@@ -50,8 +58,20 @@ public class PlayerControl : StatefulMonoBehavior<PlayerControl.States>
         }
     }
 
+    public Vector2 JukeVelocity
+    {
+        get
+        {
+            return new Vector2(Mathf.Cos(_radAngle), Mathf.Sin(_radAngle)) * 60 * Time.deltaTime;
+        }
+        set
+        {
+            _radAngle = Mathf.Atan2(value.y, value.x);
+        }
+    }
+
     // Use this for initialization
-	void Start () {
+    void Start () {
 		
 		//holding onto for reference later
 		/*
@@ -67,47 +87,23 @@ public class PlayerControl : StatefulMonoBehavior<PlayerControl.States>
 	
 	// Update is called once per frame
 	void Update () {
-		//might be needed later
-		/*if (Time.timeScale == GameControl.Paused)
+        //might be needed later
+        /*if (Time.timeScale == GameControl.Paused)
 	    {
 	        return;
 	    }*/
 
-		//left right input
-		if (Input.GetKey(MoveLeft))
-		{
-			_currentPlayerVector.x = _LEFT_X;
-		}
-		else if (Input.GetKey(MoveRight))
-		{
-			_currentPlayerVector.x = _RIGHT_X;
-		}
-		else
-		{
-			_currentPlayerVector.x = _IDLE;
-		}
-
-		//forward backward input
-		if (Input.GetKey(MoveForward))
-		{
-			_currentPlayerVector.y = _FORWARD_Y;
-		}
-		else if (Input.GetKey(MoveBackward))
-		{
-			_currentPlayerVector.y = _BACKWARD_Y;
-		}
-		else
-		{
-			_currentPlayerVector.y = _IDLE;
-		}
+        UpdatePlayerMovementInput();
+        UpdatePlayerSpecialInput();
 
 
-		Velocity = _currentPlayerVector;
+
+        Velocity = _currentPlayerVector;
 		float currentPlayerVectorX = _currentPlayerVector.x;
 		float currentPlayerVectorY = _currentPlayerVector.y;
 		_currentPlayerSpeed = (currentPlayerVectorX != 0 || currentPlayerVectorY != 0) ? PlayerBaseSpeed : PlayerIdleSpeed;
 		gameObject.transform.Translate(Velocity);
-		/*switch (State)
+        /*switch (State)
 	    {
 			case States.Idle:
 	            break;
@@ -122,8 +118,75 @@ public class PlayerControl : StatefulMonoBehavior<PlayerControl.States>
 	    }*/
 	}
 
+    void UpdatePlayerMovementInput() {
+        //left right input
+        if (Input.GetKey(MoveLeft))
+        {
+            _currentPlayerVector.x = _LEFT_X;
+        }
+        else if (Input.GetKey(MoveRight))
+        {
+            _currentPlayerVector.x = _RIGHT_X;
+        }
+        else
+        {
+            _currentPlayerVector.x = _IDLE;
+        }
+
+        //forward backward input
+        if (Input.GetKey(MoveForward))
+        {
+            _currentPlayerVector.y = _FORWARD_Y;
+        }
+        else if (Input.GetKey(MoveBackward))
+        {
+            _currentPlayerVector.y = _BACKWARD_Y;
+        }
+        else
+        {
+            _currentPlayerVector.y = _IDLE;
+        }
+    }
+
+    void UpdatePlayerSpecialInput()
+    {
+        if (Input.GetKeyDown(Spin) && _notSpinning)
+        {
+            _notSpinning = false;
+        }
+        else if (!_notSpinning)
+        {
+            transform.Rotate(0, 0, _DEGREES_SPUN_PER_UPDATE);
+            if (Mathf.Approximately(transform.rotation.z, 0f))
+            {
+                _notSpinning = true;
+            }
+        }
+
+        if (Input.GetKey(Sprint))
+        {
+            PlayerBaseSpeed = 8;
+        }
+        else
+        {
+            PlayerBaseSpeed = 4;
+        }
+
+        if (Input.GetKeyDown(Juke))
+        {
+            JukeVelocity = _currentPlayerVector;
+            gameObject.transform.Translate(JukeVelocity);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("enter collide");
+    }
+
     void OnTriggerStay2D(Collider2D other)
     {
+        Debug.Log("colliding");
         /*if (Time.timeScale == GameControl.Paused)
         {
             return;
@@ -149,6 +212,7 @@ public class PlayerControl : StatefulMonoBehavior<PlayerControl.States>
 
     void OnTriggerExit2D(Collider2D other)
     {
+        Debug.Log("exiting");
         /*WallControl wall;
         ObstacleControl obs;
 
