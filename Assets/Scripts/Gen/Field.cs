6648890253206a4dfe9e70335484fdb721c0d850
dpumford <dpumford@hotmail.com>
2018-04-23 10:00:37 -0500

    Adds edges for the player.

--- Assets/Scripts/Gen/Field.cs
+++ Assets/Scripts/Gen/Field.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Field : MonoBehaviour {
     public SpriteRenderer Endzone;
+    public BoxCollider2D EdgePrefab;
     public int YardsPerSection;
 
     public PlanExecutor Plan;
@@ -11,6 +12,9 @@ public class Field : MonoBehaviour {
     private bool _initialized;
     private SpriteRenderer _field;
     private SpriteRenderer _instanciatedEndzone;
+    private BoxCollider2D _instanciatedLeftEdge;
+    private BoxCollider2D _instanciatedRightEdge;
+    private BoxCollider2D _instanciatedBottomEdge;
 
     public float YardLength
     {
@@ -32,7 +36,11 @@ public class Field : MonoBehaviour {
 	void Start () {
         _field = GetComponent<SpriteRenderer>();
         _instanciatedEndzone = Instantiate(Endzone, Vector3.zero, Quaternion.identity);
-	}
+
+        _instanciatedLeftEdge = Instantiate(EdgePrefab, Vector3.zero, Quaternion.identity);
+        _instanciatedRightEdge = Instantiate(EdgePrefab, Vector3.zero, Quaternion.identity);
+        _instanciatedBottomEdge = Instantiate(EdgePrefab, Vector3.zero, Quaternion.identity);
+    }
 
     // Update is called once per frame
     void Update()
@@ -41,5 +49,14 @@ public class Field : MonoBehaviour {
         transform.position = new Vector3(0, _field.bounds.size.y / 2, 5);
 
         _instanciatedEndzone.transform.position = new Vector3(0, _field.bounds.size.y + Endzone.bounds.size.y / 2, 5);
+
+        _instanciatedLeftEdge.transform.position = new Vector3((_field.bounds.size.x / 2 + 0.5f) * -1, transform.position.y);
+        _instanciatedLeftEdge.size = new Vector2(1,  _field.bounds.size.y);
+
+        _instanciatedRightEdge.transform.position = new Vector3(_field.bounds.size.x / 2 + 0.5f, transform.position.y);
+        _instanciatedRightEdge.size = new Vector2(1, _field.bounds.size.y);
+
+        _instanciatedBottomEdge.transform.position = new Vector3(0, -0.5f);
+        _instanciatedBottomEdge.size = new Vector2(_field.bounds.size.x, 1);
     }
 }
